3334422d101194c5780a1e4d17accccb8f9f546c
apsatarovd <dapsatarov@icloud.com>
2024-08-03 22:11:19 +0700

    закончил проект

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -3,48 +3,40 @@ from app.models import TodoItem
 from app.models import datetime
 import json
 from pydantic import BaseModel
+
 router = APIRouter()
 
+def load_todos():
+    with open("storage.json", "r") as file:
+        todos_json = json.load(file)
+    todos = [TodoItem(**todo_json) for todo_json in todos_json]
+    return todos
+
+def save_todos(todos):
+    todos_dict = [todo.model_dump() for todo in todos]
+    with open("storage.json", "w") as file:
+        json.dump(todos_dict, file, indent=2, default=str)
 
 @router.get("/todos")
 def get_all_todos():
-    with open("storage.json", "r") as file:
-        todos_json = json.load(file)
-    todos = []
-    for todo_json in todos_json:
-        todo = TodoItem(**todo_json)
-        todos.append(todo)
+    todos = load_todos()
     return todos
 
 @router.get("/todos/{id}")
 def get_todo(id: int):
-    with open("storage.json", "r") as file:
-        todos_json = json.load(file)
-    todos = []
-    for todo_json in todos_json:
-        todo = TodoItem(**todo_json)
-        todos.append(todo)
+    todos = load_todos()
     for todo in todos:
         if todo.id == id:
             return todo
     return {"message": f"Задача с id {id} не найдена"}
 
-
-
-#новый post запрос чтобы данные шли от пользователя 
+# Новый POST-запрос, чтобы данные шли от пользователя
 class CreateTodoRequest(BaseModel):
     title: str
     description: str
-
-
 @router.post("/todos")
-def create_todo(request:CreateTodoRequest):
-    with open("storage.json", "r") as file:
-        todos_json = json.load(file)
-    todos = []
-    for todo_json in todos_json:
-        todo = TodoItem(**todo_json)
-        todos.append(todo)
+def create_todo(request: CreateTodoRequest):
+    todos = load_todos()
 
     created_at = datetime.now()
     updated_at = datetime.now()
@@ -52,60 +44,35 @@ def create_todo(request:CreateTodoRequest):
     new_todo = TodoItem(id=new_id, title=request.title, description=request.description, created_at=created_at, updated_at=updated_at)
     todos.append(new_todo)
 
-   
-    todos_dict = []
-    for todo in todos:
-        todo_dict = todo.model_dump()
-        todos_dict.append(todo_dict)
-
-    with open("storage.json", "w") as file:
-        json.dump(todos_dict, file, indent=2, default=str)
-
+    save_todos(todos)
     return {"message": f"Задача с ID {new_id} добавлена в список"}
 
-"-----------------------------------------------------------------------------------------------------------------------------------------------"
 
 
 @router.put("/todos/{id}")
 def update_todo(id: int, request: CreateTodoRequest):
-    with open("storage.json", "r") as file:
-        todos_json = json.load(file)
+    todos = load_todos()
 
-    for todo in todos_json:
-        if todo["id"] == id:
-            todo["title"] = request.title
-            todo["description"] = request.description
-            todo["updated_at"] = str(datetime.now())
-            
+    for todo in todos:
+        if todo.id == id:
+            todo.title = request.title
+            todo.description = request.description
+            todo.updated_at = datetime.now()
 
-    with open("storage.json", "w") as file:
-        json.dump(todos_json, file, indent=2, default=str)
+    save_todos(todos)
     return {"message": f"Задача с ID {id} обновлена"}
-
-
-
+    
 
 @router.delete("/todos/{id}")
 def delete_todo(id: int):
-    with open("storage.json", "r") as file:
-        todos_json = json.load(file)
-    todos = []
-    for todo_json in todos_json:
-        todo = TodoItem(**todo_json)
-        todos.append(todo)
+    todos = load_todos()
 
     for todo in todos:
         if todo.id == id:
             todos.remove(todo)
-            todos_dict = []
-            for todo in todos:
-                todo_dict = todo.model_dump()
-                todos_dict.append(todo_dict)
-
-            with open("storage.json", "w") as file:
-                json.dump(todos_dict, file, indent=2, default=str)
+            save_todos(todos)
             return {"message": f"Задача с ID {id} удалена"}
-    
+
     return {"message": f"Задача с ID {id} не найдена"}
 
 
